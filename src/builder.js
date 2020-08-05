9f7e91e7393b1d6001340ae3808930869c9a2ce1
Aaron Powell <me@aaron-powell.com>
2020-08-05 23:25:10 +1000

    Adding build support for dotnet apps

--- src/builder.js
+++ src/builder.js
@@ -23,27 +23,59 @@ const nodeBuilder = (location, buildCommand, name, colour) => {
   });
 };
 
+const dotnetBuilder = (location, name, colour) => {
+  const appBuildCommand = [
+    "CI=1",
+    concurrentlyBin,
+    `--names ${name}`,
+    `-c '${colour}'`,
+    `--kill-others-on-fail`,
+    `"dotnet build"`,
+    `--color=always`,
+  ].join(" ");
+  exec(appBuildCommand, {
+    cwd: location,
+  });
+};
+
 module.exports = () => {
   const { app_location, api_location, app_build_command, api_build_command } = readConfigFile();
   const runtimeType = detectRuntime(app_location);
 
   try {
     switch (runtimeType) {
+      case RuntimeType.dotnet:
+        {
+          // build app
+          dotnetBuilder(app_location, "app_build", "bgGreen.bold");
+
+          // NOTE: API is optional. Build it only if it exists
+          // This may result in a double-compile of some libraries if they are shared between the
+          // Blazor app and the API, but it's an acceptable outcome
+          let apiLocation = path.resolve(process.cwd(), api_location);
+          if (fs.existsSync(apiLocation) === true && fs.existsSync(path.join(apiLocation, "host.json"))) {
+            dotnetBuilder(apiLocation, "api_build", "bgYellow.bold");
+          }
+        }
+        break;
+
       case RuntimeType.node:
       default:
-        // figure out if appLocation exists
-        let appLocation = app_location;
-        if (fs.existsSync(appLocation) === false) {
-          appLocation = process.cwd();
-        }
+        {
+          // figure out if appLocation exists
+          let appLocation = app_location;
+          if (fs.existsSync(appLocation) === false) {
+            appLocation = process.cwd();
+          }
 
-        // build app
-        nodeBuilder(appLocation, app_build_command, "app_build", "bgGreen.bold");
+          // build app
+          nodeBuilder(appLocation, app_build_command, "app_build", "bgGreen.bold");
 
-        // NOTE: API is optional. Build it only if it exists
-        let apiLocation = path.resolve(process.cwd(), api_location);
-        if (fs.existsSync(apiLocation) === true && fs.existsSync(path.join(apiLocation, "host.json"))) {
-          nodeBuilder(apiLocation, api_build_command, "api_build", "bgYellow.bold");
+          // NOTE: API is optional. Build it only if it exists
+          let apiLocation = path.resolve(process.cwd(), api_location);
+          if (fs.existsSync(apiLocation) === true && fs.existsSync(path.join(apiLocation, "host.json"))) {
+            nodeBuilder(apiLocation, api_build_command, "api_build", "bgYellow.bold");
+          }
         }
         break;
     }
