4f3e6f5b4b772dd4230b5f0912897d63e9d47af9
Wassim Chegham <1699357+manekinekko@users.noreply.github.com>
2021-03-02 09:34:16 +0100

    fix: null mimeTypes
    Co-authored-by: Anthony Chu <anthony@anthonychu.ca>

--- src/proxy/routes-engine/rules/mimeTypes.ts
+++ src/proxy/routes-engine/rules/mimeTypes.ts
@@ -4,7 +4,7 @@ import http from "http";
 export const mimeTypes = async (req: http.IncomingMessage, res: http.ServerResponse, mimeTypes: SWAConfigFileMimeTypes) => {
   if (req.url?.includes(".")) {
     const fileExtentionFromURL = req.url?.split(".").pop();
-    const overrideMimeType = mimeTypes[`.${fileExtentionFromURL}`];
+    const overrideMimeType = mimeTypes?.[`.${fileExtentionFromURL}`];
 
     if (fileExtentionFromURL && overrideMimeType) {
       res.setHeader("Content-Type", overrideMimeType);
