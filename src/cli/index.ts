4c508c930a1bf3d0333c81adae58fa6c363fab42
Anthony Chu <anthony@anthonychu.ca>
2021-04-03 17:01:07 -0700

    feat: update help text
    - hide undocumented options
    - add examples
    - add documentation link
    - change order of usage (all our docs put context before options)

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,7 +3,7 @@
 const args = process.argv.slice(2);
 process.title = ["swa", ...args].join(" ");
 
-import program from "commander";
+import program, { Option } from "commander";
 import path from "path";
 import { DEFAULT_CONFIG } from "../config";
 import { parsePort } from "../core/utils";
@@ -12,14 +12,17 @@ import { start } from "./commands/start";
 (async function () {
   const cli: SWACLIConfig & program.Command = program
     .name("swa")
-    .usage("[options] <command>")
+    .usage("<command> [options]")
     .version(require("../../package.json").version, "-v, --version")
 
     // SWA config
-    .option("--verbose [prefix]", "enable verbose output. Value are: silly,info,log,silent", "log");
+    .option("--verbose [prefix]", "enable verbose output. Value are: silly,info,log,silent", "log")
+
+    .addHelpText("after", "\nDocumentation:\n  https://aka.ms/swa/cli-local-development");
 
   program
     .command("start [context]")
+    .usage("[context] [options]")
     .description("start the emulator from a directory or bind to a dev server")
     .option("--app-location <appLocation>", "set location for the static app source code", DEFAULT_CONFIG.appLocation)
     .option(
@@ -28,22 +31,27 @@ import { start } from "./commands/start";
       DEFAULT_CONFIG.outputLocation
     )
     .option("--api, --api-location <apiLocation>", "set the API folder or Azure Functions emulator address", DEFAULT_CONFIG.apiLocation)
-    .option(
-      "--routes, --routes-location <routesLocation>",
-      "set the directory location where the staticwebapp.config.json file is found. This location is relative to the root of the project",
-      DEFAULT_CONFIG.routesLocation
+    .addOption(
+      new Option(
+        "--routes, --routes-location <routesLocation>",
+        "set the directory location where the staticwebapp.config.json file is found. This location is relative to the root of the project"
+      )
+        .default(DEFAULT_CONFIG.routesLocation)
+        .hideHelp()
     )
 
     // CLI config
     .option<number>("--api-port <apiPort>", "set the API backend port", parsePort, DEFAULT_CONFIG.apiPort)
     .option("--host <host>", "set the cli host address", DEFAULT_CONFIG.host)
     .option<number>("--port <port>", "set the cli port", parsePort, DEFAULT_CONFIG.port)
-    .option("--build", "build the app and API before starting the emulator", false)
+
+    .addOption(new Option("--build", "build the app and API before starting the emulator").default(false).hideHelp())
+
     .option("--ssl", "serve the app and API over HTTPS", DEFAULT_CONFIG.ssl)
     .option("--ssl-cert <sslCertLocation>", "SSL certificate (.crt) to use for serving HTTPS", DEFAULT_CONFIG.sslCert)
     .option("--ssl-key <sslKeyLocation>", "SSL key (.key) to use for serving HTTPS", DEFAULT_CONFIG.sslKey)
 
-    .option("--run <startupScript>", "run a external program or npm/yarn script on startup", DEFAULT_CONFIG.run)
+    .addOption(new Option("--run <startupScript>", "run a external program or npm/yarn script on startup").default(DEFAULT_CONFIG.run).hideHelp())
 
     .action(async (context: string = `.${path.sep}`, options: SWACLIConfig) => {
       options = {
@@ -60,7 +68,26 @@ import { start } from "./commands/start";
       }
 
       await start(context, options);
-    });
+    })
+
+    .addHelpText(
+      "after",
+      `
+Examples:
+  
+  Serve static content from the current folder
+  swa start
+
+  Serve static content from a specific folder
+  swa start ./output-folder
+
+  Use an already running framework development server
+  swa start http://localhost:3000
+
+  Serve static content and run an API from another folder
+  swa start ./output-folder --api ./api
+    `
+    );
 
   await program.parseAsync(process.argv);
 })();
