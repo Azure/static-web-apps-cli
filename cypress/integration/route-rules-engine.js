1010ba210734269321c15598d79fb4b5b209c9b0
Wassim Chegham <github@wassim.dev>
2021-04-09 16:45:27 +0200

    chore: add more e2e for authenticated roles
    Fixes #174

--- cypress/integration/route-rules-engine.js
+++ cypress/integration/route-rules-engine.js
@@ -1,59 +1,55 @@
 /// <reference types="cypress" />
 
 context("route rules engine", { failOnStatusCode: false, defaultCommandTimeout: 20000 /* set this for Windows */ }, () => {
-  beforeEach(() => {
-    cy.visit("http://0.0.0.0:1234");
-  });
-
   it("root returns /index.html", async () => {
-    cy.request("/").then(() => {
+    cy.visit("http://0.0.0.0:1234/").should(() => {
       cy.title().should("eq", "/index.html");
     });
   });
 
   it("/index.html returns /index.html", async () => {
-    cy.request("/index.html").then(() => {
+    cy.visit("http://0.0.0.0:1234/index.html").should(() => {
       cy.title().should("eq", "/index.html");
     });
   });
 
   it("folder returns folder/index.html", async () => {
-    cy.request("/folder/").then(() => {
+    cy.visit("http://0.0.0.0:1234/folder/").should(() => {
       cy.title().should("eq", "/folder/index.html");
     });
   });
 
   it("rewrite to file returns correct content", async () => {
-    cy.request("/rewrite_index2").then(() => {
+    cy.visit("http://0.0.0.0:1234/rewrite_index2").should(() => {
       cy.title().should("eq", "/index2.html");
     });
   });
 
   it("rewrite to function returns function response", async () => {
-    cy.request("/rewrite-to-function").then((response) => {
+    cy.visit("http://0.0.0.0:1234/rewrite-to-function").should((response) => {
       expect(response).to.have.property("x-swa-custom");
       expect(response["x-swa-custom"]).to.be("/api/headers");
     });
   });
 
   it("content response contains global headers", async () => {
-    cy.request("/").then((response) => {
+    cy.visit("http://0.0.0.0:1234/").should((response) => {
       expect(response.headers.get("a")).to.be("b");
     });
   });
 
   it("route headers override global headers", async () => {
-    cy.request("/rewrite_index2").then((response) => {
+    cy.visit("http://0.0.0.0:1234/rewrite_index2").should((response) => {
       expect(response.headers.get("a")).to.be("c");
     });
   });
 
   it("default redirect returns 302 with correct location", async () => {
-    cy.request("/redirect/foo").as("response");
+    cy.visit("http://0.0.0.0:1234/redirect/foo").as("response");
 
     cy.get("@response")
       .its("headers")
-      .then((headers) => {
+      .should((headers) => {
         expect(headers).to.deep.include({
           status: "302",
           location: "http://0.0.0.0:1234/index2.html",
@@ -62,11 +58,11 @@ context("route rules engine", { failOnStatusCode: false, defaultCommandTimeout: 
   });
 
   it("redirect with statusCode 302 returns 302 with correct location", async () => {
-    cy.request("/redirect/302").as("response");
+    cy.visit("http://0.0.0.0:1234/redirect/302").as("response");
 
     cy.get("@response")
       .its("headers")
-      .then((headers) => {
+      .should((headers) => {
         expect(headers).to.deep.include({
           status: "302",
           location: "http://0.0.0.0:1234/index2.html",
@@ -75,11 +71,11 @@ context("route rules engine", { failOnStatusCode: false, defaultCommandTimeout: 
   });
 
   it("redirect with statusCode 301 returns 301 with correct location", async () => {
-    cy.request("/redirect/301").as("response");
+    cy.visit("http://0.0.0.0:1234/redirect/301").as("response");
 
     cy.get("@response")
       .its("headers")
-      .then((headers) => {
+      .should((headers) => {
         expect(headers).to.deep.include({
           status: "302",
           location: "http://0.0.0.0:1234/index2.html",
@@ -88,31 +84,31 @@ context("route rules engine", { failOnStatusCode: false, defaultCommandTimeout: 
   });
 
   it("setting mimetype of unknown file type returns correct mime type", async () => {
-    cy.request("/test.swaconfig").then((response) => {
+    cy.visit("http://0.0.0.0:1234/test.swaconfig").should((response) => {
       expect(response.status).to.be(200);
       expect(response.headers.get("content-type")).to.be("application/json");
     });
   });
 
   it("navigation fallback returns /index.html", async () => {
-    cy.request("/does_not_exist.html").then((response) => {
+    cy.visit("http://0.0.0.0:1234/does_not_exist.html").should((response) => {
       expect(response.status).to.be(200);
       cy.title().should("eq", "/index.html");
     });
   });
 
   it("navigation fallback that's excluded returns 404", async () => {
-    cy.request("/does_not_exist.txt").then((response) => {
+    cy.visit("http://0.0.0.0:1234/does_not_exist.txt").should((response) => {
       expect(response.status).to.be(404);
     });
   });
 
   it("/*.foo matches extension", async () => {
-    cy.request("/thing.foo").as("response");
+    cy.visit("http://0.0.0.0:1234/thing.foo").as("response");
 
     cy.get("@response")
       .its("headers")
-      .then((headers) => {
+      .should((headers) => {
         expect(headers).to.deep.include({
           status: "302",
           location: "http://0.0.0.0:1234/foo.html",
@@ -121,11 +117,11 @@ context("route rules engine", { failOnStatusCode: false, defaultCommandTimeout: 
   });
 
   it("/*.{jpg} matches extension", async () => {
-    cy.request("/thing.jpg").as("response");
+    cy.visit("http://0.0.0.0:1234/thing.jpg").as("response");
 
     cy.get("@response")
       .its("headers")
-      .then((headers) => {
+      .should((headers) => {
         expect(headers).to.deep.include({
           status: "302",
           location: "http://0.0.0.0:1234/jpg.html",
@@ -134,22 +130,22 @@ context("route rules engine", { failOnStatusCode: false, defaultCommandTimeout: 
   });
 
   it("/*.{png,gif} matches multiple extensions", async () => {
-    cy.request("/thing.png").as("response1");
+    cy.visit("http://0.0.0.0:1234/thing.png").as("response1");
 
     cy.get("@response1")
       .its("headers")
-      .then((headers) => {
+      .should((headers) => {
         expect(headers).to.deep.include({
           status: "302",
           location: "http://0.0.0.0:1234/png_gif.html",
         });
       });
 
-    cy.request("/thing.gif").as("response2");
+    cy.visit("http://0.0.0.0:1234/thing.gif").as("response2");
 
     cy.get("@response2")
       .its("headers")
-      .then((headers) => {
+      .should((headers) => {
         expect(headers).to.deep.include({
           status: "302",
           location: "http://0.0.0.0:1234/png_gif.html",
@@ -158,14 +154,14 @@ context("route rules engine", { failOnStatusCode: false, defaultCommandTimeout: 
   });
 
   it("redirect can redirect to external URL", async () => {
-    cy.request("/something.google").then((response) => {
+    cy.visit("http://0.0.0.0:1234/something.google").should((response) => {
       expect(response.status).to.be(302);
       expect(response.headers.get("location")).to.be("https://www.google.com/");
     });
   });
 
   it("rewrite to folder returns folder's default file", async () => {
-    cy.request("/folder/somefile.html").then((response) => {
+    cy.visit("http://0.0.0.0:1234/folder/somefile.html").should((response) => {
       expect(response.status).to.be(200);
       cy.title().should("eq", "/folder/index.html");
     });
