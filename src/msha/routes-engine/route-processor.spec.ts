46de07180cf5c38a994b531f5398c0f5cd824725
Wassim Chegham <1699357+manekinekko@users.noreply.github.com>
2022-04-04 21:37:53 +0200

    fix: improve query params logic if dev server (#430)
    Closes #429

--- src/msha/routes-engine/route-processor.spec.ts
+++ src/msha/routes-engine/route-processor.spec.ts
@@ -8,71 +8,71 @@ describe("parseQueryParams()", () => {
     it("should match /?a=1&b=2 URL", () => {
       req.url = "/?a=1&b=2";
       const matchingRouteRule = { route: "/" };
-      const { matchingRewriteRoutePath, matchingRewriteRoute } = parseQueryParams(req, matchingRouteRule);
+      const { urlPathnameWithoutQueryParams, urlPathnameWithQueryParams } = parseQueryParams(req, matchingRouteRule);
 
-      expect(matchingRewriteRoutePath).toBe("/");
-      expect(matchingRewriteRoute).toBe(req.url);
+      expect(urlPathnameWithoutQueryParams).toBe("/");
+      expect(urlPathnameWithQueryParams).toBe(req.url);
     });
 
     it("should match /index.html?a=1&b=2 URL", () => {
       req.url = "/index.html?a=1&b=2";
       const matchingRouteRule = { route: "/" };
-      const { matchingRewriteRoutePath, matchingRewriteRoute } = parseQueryParams(req, matchingRouteRule);
+      const { urlPathnameWithoutQueryParams, urlPathnameWithQueryParams } = parseQueryParams(req, matchingRouteRule);
 
-      expect(matchingRewriteRoutePath).toBe("/index.html");
-      expect(matchingRewriteRoute).toBe(req.url);
+      expect(urlPathnameWithoutQueryParams).toBe("/index.html");
+      expect(urlPathnameWithQueryParams).toBe(req.url);
     });
 
     it("should match /foo/bar/index.html?a=1&b=2 URL", () => {
       req.url = "/foo/bar/index.html?a=1&b=2";
       const matchingRouteRule = { route: "/" };
-      const { matchingRewriteRoutePath, matchingRewriteRoute } = parseQueryParams(req, matchingRouteRule);
+      const { urlPathnameWithoutQueryParams, urlPathnameWithQueryParams } = parseQueryParams(req, matchingRouteRule);
 
-      expect(matchingRewriteRoutePath).toBe("/foo/bar/index.html");
-      expect(matchingRewriteRoute).toBe(req.url);
+      expect(urlPathnameWithoutQueryParams).toBe("/foo/bar/index.html");
+      expect(urlPathnameWithQueryParams).toBe(req.url);
     });
 
     it("should match /foo/bar/?a=1&b=2 URL", () => {
       req.url = "/foo/bar/?a=1&b=2";
       const matchingRouteRule = { route: "/" };
-      const { matchingRewriteRoutePath, matchingRewriteRoute } = parseQueryParams(req, matchingRouteRule);
+      const { urlPathnameWithoutQueryParams, urlPathnameWithQueryParams } = parseQueryParams(req, matchingRouteRule);
 
-      expect(matchingRewriteRoutePath).toBe("/foo/bar/");
-      expect(matchingRewriteRoute).toBe(req.url);
+      expect(urlPathnameWithoutQueryParams).toBe("/foo/bar/");
+      expect(urlPathnameWithQueryParams).toBe(req.url);
     });
   });
 
   describe("with rewrite rule enabled", () => {
     it("should match /?a=1&b=2 URL", () => {
       const matchingRouteRule = { route: "/", rewrite: "/?a=1&b=2" };
-      const { matchingRewriteRoutePath, matchingRewriteRoute } = parseQueryParams(req, matchingRouteRule);
+      const { urlPathnameWithoutQueryParams, urlPathnameWithQueryParams } = parseQueryParams(req, matchingRouteRule);
 
-      expect(matchingRewriteRoutePath).toBe("/");
-      expect(matchingRewriteRoute).toBe(matchingRouteRule.rewrite);
+      expect(urlPathnameWithoutQueryParams).toBe("/");
+      expect(urlPathnameWithQueryParams).toBe(matchingRouteRule.rewrite);
     });
 
     it("should match /index.html?a=1&b=2 URL", () => {
       const matchingRouteRule = { route: "/", rewrite: "/index.html?a=1&b=2" };
-      const { matchingRewriteRoutePath, matchingRewriteRoute } = parseQueryParams(req, matchingRouteRule);
+      const { urlPathnameWithoutQueryParams, urlPathnameWithQueryParams } = parseQueryParams(req, matchingRouteRule);
 
-      expect(matchingRewriteRoutePath).toBe("/index.html");
-      expect(matchingRewriteRoute).toBe(matchingRouteRule.rewrite);
+      expect(urlPathnameWithoutQueryParams).toBe("/index.html");
+      expect(urlPathnameWithQueryParams).toBe(matchingRouteRule.rewrite);
     });
 
     it("should match /foo/bar/index.html?a=1&b=2 URL", () => {
       const matchingRouteRule = { route: "/", rewrite: "/foo/bar/index.html?a=1&b=2" };
-      const { matchingRewriteRoutePath, matchingRewriteRoute } = parseQueryParams(req, matchingRouteRule);
+      const { urlPathnameWithoutQueryParams, urlPathnameWithQueryParams } = parseQueryParams(req, matchingRouteRule);
 
-      expect(matchingRewriteRoutePath).toBe("/foo/bar/index.html");
-      expect(matchingRewriteRoute).toBe(matchingRouteRule.rewrite);
+      expect(urlPathnameWithoutQueryParams).toBe("/foo/bar/index.html");
+      expect(urlPathnameWithQueryParams).toBe(matchingRouteRule.rewrite);
     });
 
     it("should match /foo/bar/?a=1&b=2 URL", () => {
       const matchingRouteRule = { route: "/", rewrite: "/foo/bar/?a=1&b=2" };
-      const { matchingRewriteRoutePath, matchingRewriteRoute } = parseQueryParams(req, matchingRouteRule);
+      const { urlPathnameWithoutQueryParams, urlPathnameWithQueryParams } = parseQueryParams(req, matchingRouteRule);
 
-      expect(matchingRewriteRoutePath).toBe("/foo/bar/");
-      expect(matchingRewriteRoute).toBe(matchingRouteRule.rewrite);
+      expect(urlPathnameWithoutQueryParams).toBe("/foo/bar/");
+      expect(urlPathnameWithQueryParams).toBe(matchingRouteRule.rewrite);
     });
   });
 });
