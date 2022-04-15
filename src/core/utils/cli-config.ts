349a160cca54dffd03cd708dca6c34cdf655952d
Wassim Chegham <github@wassim.dev>
2022-04-15 13:41:15 +0200

    chore: store project details in config file

--- src/core/utils/cli-config.ts
+++ src/core/utils/cli-config.ts
@@ -1,23 +1,32 @@
+import chalk from "chalk";
+import { existsSync, promises as fsPromises } from "fs";
 import * as path from "path";
 import * as process from "process";
-import { existsSync, promises as fsPromises } from "fs";
-import { logger } from "./logger";
 import { DEFAULT_CONFIG } from "../../config";
+import { logger } from "./logger";
+const { readFile, writeFile } = fsPromises;
 
 export const swaCliConfigSchemaUrl = "https://aka.ms/azure/static-web-apps-cli/schema";
 export const swaCliConfigFilename = "swa-cli.config.json";
-const { readFile, writeFile } = fsPromises;
 
-export let currentConfig: SWACLIConfigInfo | undefined;
+/**
+ * Holds the current configuration of the CLI loaded from the `swa-cli.config.json` file.
+ */
+let currentSwaCliConfigFromFile: SWACLIConfigInfo | undefined;
 
-export const configExists = (configFilePath: string) => existsSync(configFilePath);
+export const getCurrentSwaCliConfigFromFile = () => currentSwaCliConfigFromFile;
+
+export const swaCliConfigFileExists = (configFilePath: string) => existsSync(configFilePath);
 
 export async function getConfigFileOptions(
   contextOrConfigEntry: string | undefined,
   configFilePath: string
 ): Promise<SWACLIConfig & { context?: string }> {
+  logger.silly(`Getting config file options from ${configFilePath}...`);
+
   configFilePath = path.resolve(configFilePath);
-  if (!configExists(configFilePath)) {
+  if (!swaCliConfigFileExists(configFilePath)) {
+    logger.silly(`Config file does not exist at ${configFilePath}`);
     return {};
   }
 
@@ -31,6 +40,8 @@ export async function getConfigFileOptions(
   const configDir = path.dirname(configFilePath);
   process.chdir(configDir);
 
+  logger.silly(`Changed directory to ${configDir}`);
+
   if (contextOrConfigEntry === DEFAULT_CONFIG.outputLocation) {
     const hasMultipleConfig = Object.entries(cliConfig.configurations).length > 1;
     if (hasMultipleConfig) {
@@ -41,20 +52,25 @@ export async function getConfigFileOptions(
 
     const [configName, config] = Object.entries(cliConfig.configurations)[0];
     printConfigMsg(configName, configFilePath);
-    currentConfig = {
+    currentSwaCliConfigFromFile = {
       name: configName,
       filePath: configFilePath,
-      config
-    }
+      config,
+    };
     return { ...config };
+  } else {
+    logger.silly(`Configuration="${contextOrConfigEntry}" does't match outputLocation="${DEFAULT_CONFIG.outputLocation}"`);
   }
 
   if (contextOrConfigEntry === undefined) {
+    logger.warn(`No configuration specified. Ignoring "${swaCliConfigFilename}"`);
     return {};
   }
 
   const config = cliConfig.configurations?.[contextOrConfigEntry];
   if (config) {
+    logger.silly(`Found configuration "${contextOrConfigEntry}" in "${swaCliConfigFilename}"`);
+
     printConfigMsg(contextOrConfigEntry, configFilePath);
     return { ...config };
   }
@@ -76,7 +92,7 @@ async function tryParseSwaCliConfig(file: string) {
 
 function printConfigMsg(name: string, file: string) {
   logger.log(`Using configuration "${name}" from file:`);
-  logger.log(`  ${file}`);
+  logger.log(chalk.green(`  ${file}`));
   logger.log("");
 }
 
@@ -85,11 +101,14 @@ export async function hasConfigurationNameInConfigFile(configFilePath: string, n
   return configJson.configurations?.[name] !== undefined;
 }
 
-export async function updateCurrentConfigFile(config: SWACLIConfig) {
-  if (currentConfig === undefined) {
-    throw new Error("No configuration file currently loaded");
+export async function updateSwaCliConfigFile(config: SWACLIConfig) {
+  if (currentSwaCliConfigFromFile === undefined) {
+    logger.error("No configuration file currently loaded", true);
+  } else {
+    logger.silly(`Updating configuration file at ${currentSwaCliConfigFromFile.filePath}`);
+
+    await writeConfigFile(currentSwaCliConfigFromFile.filePath, currentSwaCliConfigFromFile.name, config);
   }
-  await writeConfigFile(currentConfig.filePath, currentConfig.name, config);
 }
 
 export async function writeConfigFile(configFilePath: string, configName: string, config: SWACLIConfig) {
@@ -99,7 +118,9 @@ export async function writeConfigFile(configFilePath: string, configName: string
     configurations: {},
   };
 
-  if (configExists(configFilePath)) {
+  if (swaCliConfigFileExists(configFilePath)) {
+    logger.silly(`Loading existing swa-cli.config.json file at ${configFilePath}`);
+
     try {
       const configJson = await readFile(configFilePath, "utf-8");
       configFile = JSON.parse(configJson) as SWACLIConfigFile;
@@ -109,17 +130,21 @@ export async function writeConfigFile(configFilePath: string, configName: string
         logger.error(error);
       }
       logger.error("Cannot update existing configuration file.");
-      logger.error("Please fix or remove your swa-cli.config.json file and try again.");
+      logger.error("Please fix or delete your swa-cli.config.json file and try again.");
       return;
     }
   }
 
   if (configFile.configurations === undefined) {
+    logger.silly(`Creating "configurations" property in swa-cli.config.json file at ${configFilePath}`);
     configFile.configurations = {};
   }
 
   configFile.configurations[configName] = config;
   try {
+    logger.silly(`Writing configuration "${configName}" to swa-cli.config.json`);
+    logger.silly(config);
+
     await writeFile(configFilePath, JSON.stringify(configFile, null, 2));
   } catch (error) {
     logger.error(`Error writing configuration to ${configFilePath}`);
