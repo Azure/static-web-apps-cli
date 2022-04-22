1ec32ab3f8ad5b8746db7f0a399148aa1404ea46
sinedied <noda@free.fr>
2022-04-22 10:14:45 +0200

    feat: apply command-specific options

--- src/cli/commands/start.ts
+++ src/cli/commands/start.ts
@@ -59,8 +59,8 @@ export default function registerCommand(program: Command) {
       console.warn(chalk.yellow("************************************************************************"));
       console.warn();
 
-      const config = await configureOptions(outputLocation, command.optsWithGlobals(), command);
-      await start(outputLocation, config.options);
+      const config = await configureOptions(outputLocation, command.optsWithGlobals(), command, "start");
+      await start(config.outputLocation ?? outputLocation, config.options);
     })
     .addHelpText(
       "after",
