053a02bbd0ec16d9dbed413da968a44b8b1e00fc
sinedied <noda@free.fr>
2022-04-25 10:16:03 +0200

    refactor: move safeReadJson and add tests

--- src/core/utils/strings.ts
+++ src/core/utils/strings.ts
@@ -1,3 +1,6 @@
+import { promises as fs } from "fs";
+import { logger } from "./logger";
+
 export function dasherize(str: string) {
   return str
     .replace(/([a-z\d])([A-Z]+)/g, "$1-$2")
@@ -10,4 +13,15 @@ export function stripJsonComments(json: string) {
     /\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/gm,
     (match, group) => group ? "" : match
   );
+}
+
+export async function safeReadJson(path: string): Promise<JsonData | undefined> {
+  try {
+    let contents = await fs.readFile(path, 'utf8');
+    contents = stripJsonComments(contents);
+    return JSON.parse(contents) as JsonData;
+  } catch (error) {
+    logger.warn(`Failed to read JSON file at: ${path}`);
+    return undefined;
+  }
 }
