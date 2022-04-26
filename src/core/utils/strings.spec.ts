ed94795d7f17fa59a68100b99dfca2df3066ee75
sinedied <noda@free.fr>
2022-04-26 15:50:27 +0200

    refactor: add utils/file

--- src/core/utils/strings.spec.ts
+++ src/core/utils/strings.spec.ts
@@ -1,5 +1,4 @@
-import { dasherize, stripJsonComments, safeReadJson, safeReadFile } from "./strings";
-import mockFs from "mock-fs";
+import { dasherize, stripJsonComments } from "./strings";
 
 describe("dasherize()", () => {
   it("should convert to dash case", () => {
@@ -34,43 +33,5 @@ describe("stripJsonComments()", () => {
       
       "but": "not // this or /* this */ or /* this"
     }`);
-  });
-});
-
-describe("safeReadJson()", () => {
-  afterEach(() => {
-    mockFs.restore();
-  });
-
-  it("should return undefined when trying to read an invalid JSON file", async () => {
-    mockFs({ "test.json": "{ invalid json" });
-    expect(await safeReadJson("test.json")).toBe(undefined);
-  });
-
-  it("should ignore JSON comments and parse JSON anyway", async () => {
-    mockFs({ "test.json": `{
-      // this is a /* tricky comment
-      "hello": "world",
-      /* and this should be removed too // */
-      "but": "not // this or /* this */ or /* this"
-    }`});
-    const json = await safeReadJson("test.json");
-    expect(json && json.hello).toBe("world");
-  });
-});
-
-describe("safeReadFile()", () => {
-  afterEach(() => {
-    mockFs.restore();
-  });
-
-  it("should return undefined when trying to read a file that doesn't exist", async () => {
-    mockFs();
-    expect(await safeReadFile("test.txt")).toBe(undefined);
-  });
-
-  it("should return file contents", async () => {
-    mockFs({ "test.txt": "hello" });
-    expect(await safeReadFile("test.txt")).toBe("hello");
   });
 });
