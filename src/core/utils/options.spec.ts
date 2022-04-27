dd402552007cf294b2376ee534b2da36c7ce219b
Devansu <devansuyadav@gmail.com>
2022-04-27 15:30:35 +0000

    feat(cli): add alias for swa cli start options

--- src/core/utils/options.spec.ts
+++ src/core/utils/options.spec.ts
@@ -2,6 +2,7 @@ import { Command } from "commander";
 import mockFs from "mock-fs";
 import { swaCliConfigFilename } from "./cli-config";
 import { parsePort } from "./net";
+import { parseDevserverTimeout } from "./cli";
 import { configureOptions, getUserOptions } from "./options";
 import { DEFAULT_CONFIG } from "../../config";
 
@@ -94,13 +95,13 @@ describe("configureOptions()", () => {
 });
 
 describe("Testing aliases for each of the commands and their options", () => {
-  it("should return appropriate user cli options for the alias commands for global options", async () => {
+  it("should return appropriate user cli options for the alias commands for swa global options", async () => {
     const command = await new Command()
       .name("swa")
       .option("-V, --verbose [prefix]", "", DEFAULT_CONFIG.verbose)
-      .option("-c, --config <path>", "")
+      .option("-c, --config <path>")
       .option("-g, --print-config", "", false)
-      .option("-w, --swa-config-location <swaConfigLocation>", "")
+      .option("-w, --swa-config-location <swaConfigLocation>")
       .parseAsync(["node", "swa", "-V", "log", "-c", `../${swaCliConfigFilename}`, "-g", "-w", `${DEFAULT_CONFIG.swaConfigLocation}`]);
 
     expect(getUserOptions(command)).toStrictEqual({
@@ -108,6 +109,64 @@ describe("Testing aliases for each of the commands and their options", () => {
       config: `../${swaCliConfigFilename}`,
       printConfig: true,
       swaConfigLocation: DEFAULT_CONFIG.swaConfigLocation,
+    });
+  });
+
+  it("should return appropriate user cli options for the alias commands for swa start options", async () => {
+    const command = await new Command()
+      .name("swa")
+      .option("-a, --app-location <appLocation>")
+      .option("-i, --api-location <apiLocation>")
+      .option<number>("-j, --api-port <apiPort>", "", parsePort, DEFAULT_CONFIG.apiPort)
+      .option("-q, --host <host>")
+      .option<number>("-p, --port <port>", "", parsePort, DEFAULT_CONFIG.port)
+      .option("-s, --ssl")
+      .option("-e, --ssl-cert <sslCertLocation>")
+      .option("-k, --ssl-key <sslKeyLocation>")
+      .option("-r, --run <startupScript>")
+      .option<number>("-t, --devserver-timeout <devserverTimeout>", "", parseDevserverTimeout, DEFAULT_CONFIG.devserverTimeout)
+      .option("-o, --open")
+      .option("-f, --func-args <funcArgs>")
+      .parseAsync([
+        "node",
+        "swa",
+        "-a",
+        `${DEFAULT_CONFIG.appLocation}`,
+        "-i",
+        "./api",
+        "-j",
+        "7071",
+        "-q",
+        `${DEFAULT_CONFIG.host}`,
+        "-p",
+        "4567",
+        "-s",
+        "-e",
+        "./ssl/sslCert.crt",
+        "-k",
+        "./ssl/sslKey.key",
+        "-r",
+        "run.sh",
+        "-t",
+        "1000",
+        "-o",
+        "-f",
+        "--arg",
+      ]);
+
+    expect(getUserOptions(command)).toStrictEqual({
+      appLocation: DEFAULT_CONFIG.appLocation,
+      apiLocation: "./api",
+      apiPort: 7071,
+      host: DEFAULT_CONFIG.host,
+      port: 4567,
+      ssl: true,
+      sslCert: "./ssl/sslCert.crt",
+      sslKey: "./ssl/sslKey.key",
+      run: "run.sh",
+      devserverTimeout: 1000,
+      open: true,
+      funcArgs: "--arg",
     });
   });
 });
