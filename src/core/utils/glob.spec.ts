385b1fe1d4b9715564822262ee1b95c0a88fd999
sinedied <noda@free.fr>
2022-05-16 15:33:42 +0200

    test: fix unit tests

--- src/core/utils/glob.spec.ts
+++ src/core/utils/glob.spec.ts
@@ -1,118 +1,120 @@
 import { globToRegExp, isBalancedCurlyBrackets, isValidGlobExpression } from "./glob";
 
-describe("globToRegExp()", () => {
-  it("glob = <empty>", () => {
-    expect(globToRegExp("")).toBe("");
+describe("glob functions", () => {
+  describe("globToRegExp()", () => {
+    it("glob = <empty>", () => {
+      expect(globToRegExp("")).toBe("");
+    });
+
+    it("glob = abc", () => {
+      expect(globToRegExp("abc")).toBe("abc");
+    });
+
+    it("glob = foo=bar", () => {
+      expect(globToRegExp("foo=bar")).toBe("foo=bar");
+    });
+
+    it("glob = *", () => {
+      expect(globToRegExp("*")).toBe("*");
+    });
+    it("glob = /*", () => {
+      expect(globToRegExp("/*")).toBe("\\/.*");
+    });
+
+    it("glob = /foo/*", () => {
+      expect(globToRegExp("/foo/*")).toBe("\\/foo\\/.*");
+    });
+
+    it("glob = /*.{ext}", () => {
+      expect(globToRegExp("/*.{ext}")).toBe("\\/.*(ext)");
+    });
+
+    it("glob = /*.{ext,gif}", () => {
+      expect(globToRegExp("/*.{ext,gif}")).toBe("\\/.*(ext|gif)");
+    });
+
+    it("glob = /foo/*.{ext,gif}", () => {
+      expect(globToRegExp("/foo/*.{ext,gif}")).toBe("\\/foo\\/.*(ext|gif)");
+    });
+
+    it("glob = {foo,bar}.json", () => {
+      expect(globToRegExp("{foo,bar}.json")).toBe("(foo|bar).json");
+    });
   });
 
-  it("glob = abc", () => {
-    expect(globToRegExp("abc")).toBe("abc");
-  });
+  // describe isValidGlobExpression
 
-  it("glob = foo=bar", () => {
-    expect(globToRegExp("foo=bar")).toBe("foo=bar");
-  });
+  describe("isValidGlobExpression()", () => {
+    // valid expressions
+    ["*", "/*", "/foo/*", "/foo/*.ext", "/*.ext", "*.ext", "/foo/*.ext", "/foo/*.{ext}", "/foo/*.{ext,ext}"].forEach((glob) => {
+      describe("should be TRUE for the following values", () => {
+        it(`glob = ${glob}`, () => {
+          expect(isValidGlobExpression(glob)).toBe(true);
+        });
+      });
+    });
 
-  it("glob = *", () => {
-    expect(globToRegExp("*")).toBe("*");
-  });
-  it("glob = /*", () => {
-    expect(globToRegExp("/*")).toBe("\\/.*");
-  });
-
-  it("glob = /foo/*", () => {
-    expect(globToRegExp("/foo/*")).toBe("\\/foo\\/.*");
-  });
-
-  it("glob = /*.{ext}", () => {
-    expect(globToRegExp("/*.{ext}")).toBe("\\/.*(ext)");
-  });
-
-  it("glob = /*.{ext,gif}", () => {
-    expect(globToRegExp("/*.{ext,gif}")).toBe("\\/.*(ext|gif)");
-  });
-
-  it("glob = /foo/*.{ext,gif}", () => {
-    expect(globToRegExp("/foo/*.{ext,gif}")).toBe("\\/foo\\/.*(ext|gif)");
-  });
-
-  it("glob = {foo,bar}.json", () => {
-    expect(globToRegExp("{foo,bar}.json")).toBe("(foo|bar).json");
-  });
-});
-
-// describe isValidGlobExpression
-
-describe("isValidGlobExpression()", () => {
-  // valid expressions
-  ["*", "/*", "/foo/*", "/foo/*.ext", "/*.ext", "*.ext", "/foo/*.ext", "/foo/*.{ext}", "/foo/*.{ext,ext}"].forEach((glob) => {
-    describe("should be TRUE for the following values", () => {
-      it(`glob = ${glob}`, () => {
-        expect(isValidGlobExpression(glob)).toBe(true);
+    // invalid expressions
+    [
+      undefined,
+      "",
+      "*.*",
+      "**.*",
+      "**.**",
+      "**",
+      "*/*",
+      "*/*.ext",
+      "*.ext/*",
+      "*/*.ext*",
+      "*.ext/*.ext",
+      "/blog/*/management",
+      "/foo/*.{ext,,,,}",
+      "/foo/*.{ext,",
+      "/foo/*.ext,}",
+      "/foo/*.ext}",
+      "/foo/*.{}",
+      "/foo/*.",
+      "/foo/.",
+    ].forEach((glob) => {
+      describe("should be FALSE for the following values", () => {
+        it(`glob = ${glob}`, () => {
+          expect(isValidGlobExpression(glob)).toBe(false);
+        });
       });
     });
   });
 
-  // invalid expressions
-  [
-    undefined,
-    "",
-    "*.*",
-    "**.*",
-    "**.**",
-    "**",
-    "*/*",
-    "*/*.ext",
-    "*.ext/*",
-    "*/*.ext*",
-    "*.ext/*.ext",
-    "/blog/*/management",
-    "/foo/*.{ext,,,,}",
-    "/foo/*.{ext,",
-    "/foo/*.ext,}",
-    "/foo/*.ext}",
-    "/foo/*.{}",
-    "/foo/*.",
-    "/foo/.",
-  ].forEach((glob) => {
-    describe("should be FALSE for the following values", () => {
-      it(`glob = ${glob}`, () => {
-        expect(isValidGlobExpression(glob)).toBe(false);
-      });
+  describe("isBalancedCurlyBrackets()", () => {
+    it("should be true for {}", () => {
+      expect(isBalancedCurlyBrackets("{,,,}")).toBe(true);
     });
-  });
-});
 
-describe("isBalancedCurlyBrackets()", () => {
-  it("should be true for {}", () => {
-    expect(isBalancedCurlyBrackets("{,,,}")).toBe(true);
-  });
+    it("should be true for {}{}{}", () => {
+      expect(isBalancedCurlyBrackets("{,,,}{,,,}{,,,}")).toBe(true);
+    });
 
-  it("should be true for {}{}{}", () => {
-    expect(isBalancedCurlyBrackets("{,,,}{,,,}{,,,}")).toBe(true);
-  });
+    it("should be true for {{}}", () => {
+      expect(isBalancedCurlyBrackets("{,,,{,,,},,,}")).toBe(true);
+    });
 
-  it("should be true for {{}}", () => {
-    expect(isBalancedCurlyBrackets("{,,,{,,,},,,}")).toBe(true);
-  });
+    it("should be false for }{", () => {
+      expect(isBalancedCurlyBrackets("},,,{")).toBe(false);
+    });
 
-  it("should be false for }{", () => {
-    expect(isBalancedCurlyBrackets("},,,{")).toBe(false);
-  });
+    it("should be false for }{}{", () => {
+      expect(isBalancedCurlyBrackets("},,,{,,,},,,{")).toBe(false);
+    });
 
-  it("should be false for }{}{", () => {
-    expect(isBalancedCurlyBrackets("},,,{,,,},,,{")).toBe(false);
-  });
+    it("should be false for {", () => {
+      expect(isBalancedCurlyBrackets("{,,,")).toBe(false);
+    });
 
-  it("should be false for {", () => {
-    expect(isBalancedCurlyBrackets("{,,,")).toBe(false);
-  });
+    it("should be false for }", () => {
+      expect(isBalancedCurlyBrackets(",,,}")).toBe(false);
+    });
 
-  it("should be false for }", () => {
-    expect(isBalancedCurlyBrackets(",,,}")).toBe(false);
-  });
-
-  it("should be false for {}}{{}", () => {
-    expect(isBalancedCurlyBrackets("{,,,}},,,{{,,,}")).toBe(false);
+    it("should be false for {}}{{}", () => {
+      expect(isBalancedCurlyBrackets("{,,,}},,,{{,,,}")).toBe(false);
+    });
   });
 });
--- src/core/utils/net.spec.ts
+++ src/core/utils/net.spec.ts
@@ -2,298 +2,300 @@ jest.mock("../constants", () => {});
 import { logger } from "./logger";
 import { address, hostnameToIpAdress, parsePort, response } from "./net";
 
-describe("response()", () => {
-  it("context = null", () => {
-    expect(() => {
-      response({
-        context: null,
-      });
-    }).toThrow();
-  });
-  it("context.bindingData = {foo:bar}", () => {
-    expect(
-      response({
-        status: 200,
-        context: {
-          bindingData: {
-            foo: "bar",
-          },
-        },
-      })
-    ).toEqual({
-      body: null,
-      cookies: undefined,
-      headers: { "Content-Type": "application/json", status: 200 },
-      status: 200,
+describe("net utilities", () => {
+  describe("response()", () => {
+    it("context = null", () => {
+      expect(() => {
+        response({
+          context: null,
+        });
+      }).toThrow();
     });
-  });
+    it("context.bindingData = {foo:bar}", () => {
+      expect(
+        response({
+          status: 200,
+          context: {
+            bindingData: {
+              foo: "bar",
+            },
+          },
+        })
+      ).toEqual({
+        body: null,
+        cookies: undefined,
+        headers: { "Content-Type": "application/json", status: 200 },
+        status: 200,
+      });
+    });
 
-  it("status = null", () => {
-    expect(() => {
-      response({
+    it("status = null", () => {
+      expect(() => {
+        response({
+          context: {
+            bindingData: {},
+          },
+          status: null,
+        });
+      }).toThrow(/TypeError/);
+    });
+
+    it("status = 200", () => {
+      const res = response({
         context: {
           bindingData: {},
         },
-        status: null,
+        status: 200,
       });
-    }).toThrow(/TypeError/);
-  });
-
-  it("status = 200", () => {
-    const res = response({
-      context: {
-        bindingData: {},
-      },
-      status: 200,
-    });
-    expect(res.status).toBe(200);
-    expect(res.headers.status).toBe(200);
-  });
-
-  it("body = null", () => {
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      body: null,
-    });
-    expect(res.body).toBe(null);
-  });
-
-  it("body = {foo:bar}", () => {
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      body: {
-        foo: "bar",
-      },
-    });
-    expect(typeof res.body).toBe("object");
-    expect(res.body.foo).toBeDefined();
-    expect(res.body.foo).toBe("bar");
-  });
-
-  it("body = {foo:bar} (DEBUG on)", () => {
-    process.env.SWA_CLI_DEBUG = "*";
-
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      body: {
-        foo: "bar",
-      },
-    });
-    expect(typeof res.body).toBe("object");
-    expect(res.body.foo).toBeDefined();
-    expect(res.body.foo).toBe("bar");
-  });
-
-  it("headers = null", () => {
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      headers: null,
-    });
-    expect(res.headers).toBeDefined();
-    expect(res.headers.status).toBe(200);
-    expect(res.headers["Content-Type"]).toBe("application/json");
-  });
-
-  it("headers = null (DEBUG on)", () => {
-    process.env.SWA_CLI_DEBUG = "*";
-
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      headers: null,
-    });
-    expect(res.headers).toBeDefined();
-    expect(res.headers.status).toBe(200);
-    expect(res.headers["Content-Type"]).toBe("application/json");
-  });
-
-  it("headers = { foo: bar }", () => {
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      headers: {
-        foo: "bar",
-      },
-    });
-    expect(res.headers).toBeDefined();
-    expect(res.headers.foo).toBe("bar");
-    expect(res.headers.status).toBe(200);
-    expect(res.headers["Content-Type"]).toBe("application/json");
-  });
-
-  it("headers = { foo: bar } (DEBUG on)", () => {
-    process.env.SWA_CLI_DEBUG = "*";
-
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      headers: {
-        foo: "bar",
-      },
-    });
-    expect(res.headers).toBeDefined();
-    expect(res.headers.foo).toBe("bar");
-    expect(res.headers.status).toBe(200);
-    expect(res.headers["Content-Type"]).toBe("application/json");
-  });
-
-  it("headers = { location: null }", () => {
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      headers: {
-        location: null,
-      },
-    });
-    expect(res.headers).toBeDefined();
-    expect(res.headers.location).toBe(null);
-    expect(res.headers.status).toBe(200);
-    expect(res.headers["Content-Type"]).toBe("application/json");
-  });
-
-  it("headers = { location: null } (DEBUG on)", () => {
-    process.env.SWA_CLI_DEBUG = "*";
-
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      headers: {
-        location: null,
-      },
-    });
-    expect(res.headers).toBeDefined();
-    expect(res.headers.location).toBe(null);
-    expect(res.headers.status).toBe(200);
-    expect(res.headers["Content-Type"]).toBe("application/json");
-  });
-
-  it("headers = { location: 'wassim.dev' }", () => {
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      headers: {
-        location: "wassim.dev",
-      },
-    });
-    expect(res.headers).toBeDefined();
-    expect(res.headers.location).toBe("wassim.dev");
-    expect(res.headers.status).toBe(200);
-    expect(res.headers["Content-Type"]).toBe("application/json");
-  });
-
-  it("cookies = null", () => {
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      cookies: null,
+      expect(res.status).toBe(200);
+      expect(res.headers.status).toBe(200);
     });
 
-    expect(res.cookies).toBe(null);
-  });
-
-  it("cookies = { foo:bar }", () => {
-    const res = response({
-      status: 200,
-      context: {
-        bindingData: {},
-      },
-      cookies: {
-        foo: "bar",
-      },
+    it("body = null", () => {
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        body: null,
+      });
+      expect(res.body).toBe(null);
     });
-    expect(res.cookies).toBeDefined();
-    expect(res.cookies!.foo).toBe("bar");
-  });
-});
 
-describe("parsePort()", () => {
-  const mockLoggerError = jest.spyOn(logger, "error").mockImplementation(() => {
-    return undefined as never;
+    it("body = {foo:bar}", () => {
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        body: {
+          foo: "bar",
+        },
+      });
+      expect(typeof res.body).toBe("object");
+      expect(res.body.foo).toBeDefined();
+      expect(res.body.foo).toBe("bar");
+    });
+
+    it("body = {foo:bar} (DEBUG on)", () => {
+      process.env.SWA_CLI_DEBUG = "*";
+
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        body: {
+          foo: "bar",
+        },
+      });
+      expect(typeof res.body).toBe("object");
+      expect(res.body.foo).toBeDefined();
+      expect(res.body.foo).toBe("bar");
+    });
+
+    it("headers = null", () => {
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        headers: null,
+      });
+      expect(res.headers).toBeDefined();
+      expect(res.headers.status).toBe(200);
+      expect(res.headers["Content-Type"]).toBe("application/json");
+    });
+
+    it("headers = null (DEBUG on)", () => {
+      process.env.SWA_CLI_DEBUG = "*";
+
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        headers: null,
+      });
+      expect(res.headers).toBeDefined();
+      expect(res.headers.status).toBe(200);
+      expect(res.headers["Content-Type"]).toBe("application/json");
+    });
+
+    it("headers = { foo: bar }", () => {
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        headers: {
+          foo: "bar",
+        },
+      });
+      expect(res.headers).toBeDefined();
+      expect(res.headers.foo).toBe("bar");
+      expect(res.headers.status).toBe(200);
+      expect(res.headers["Content-Type"]).toBe("application/json");
+    });
+
+    it("headers = { foo: bar } (DEBUG on)", () => {
+      process.env.SWA_CLI_DEBUG = "*";
+
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        headers: {
+          foo: "bar",
+        },
+      });
+      expect(res.headers).toBeDefined();
+      expect(res.headers.foo).toBe("bar");
+      expect(res.headers.status).toBe(200);
+      expect(res.headers["Content-Type"]).toBe("application/json");
+    });
+
+    it("headers = { location: null }", () => {
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        headers: {
+          location: null,
+        },
+      });
+      expect(res.headers).toBeDefined();
+      expect(res.headers.location).toBe(null);
+      expect(res.headers.status).toBe(200);
+      expect(res.headers["Content-Type"]).toBe("application/json");
+    });
+
+    it("headers = { location: null } (DEBUG on)", () => {
+      process.env.SWA_CLI_DEBUG = "*";
+
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        headers: {
+          location: null,
+        },
+      });
+      expect(res.headers).toBeDefined();
+      expect(res.headers.location).toBe(null);
+      expect(res.headers.status).toBe(200);
+      expect(res.headers["Content-Type"]).toBe("application/json");
+    });
+
+    it("headers = { location: 'wassim.dev' }", () => {
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        headers: {
+          location: "wassim.dev",
+        },
+      });
+      expect(res.headers).toBeDefined();
+      expect(res.headers.location).toBe("wassim.dev");
+      expect(res.headers.status).toBe(200);
+      expect(res.headers["Content-Type"]).toBe("application/json");
+    });
+
+    it("cookies = null", () => {
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        cookies: null,
+      });
+
+      expect(res.cookies).toBe(null);
+    });
+
+    it("cookies = { foo:bar }", () => {
+      const res = response({
+        status: 200,
+        context: {
+          bindingData: {},
+        },
+        cookies: {
+          foo: "bar",
+        },
+      });
+      expect(res.cookies).toBeDefined();
+      expect(res.cookies!.foo).toBe("bar");
+    });
   });
 
-  it("Ports below 1024 should be invalid", () => {
-    parsePort("0");
-    expect(mockLoggerError).toBeCalled();
-  });
-  it("Ports above 49151 should be invalid", () => {
-    parsePort("98765");
-    expect(mockLoggerError).toBeCalled();
-  });
-  it("Non-number ports should be invalid", () => {
-    parsePort("not a number");
-    expect(mockLoggerError).toBeCalled();
-  });
-  it("Ports between 1024 - 49151 should be valid", () => {
-    const port = parsePort("1984");
-    expect(port).toBe(1984);
-  });
-});
+  describe("parsePort()", () => {
+    const mockLoggerError = jest.spyOn(logger, "error").mockImplementation(() => {
+      return undefined as never;
+    });
 
-describe("address()", () => {
-  it("should throw for malformed URI", () => {
-    expect(() => address("", undefined)).toThrowError(/Host value is not set/);
-    expect(() => address("", 80)).toThrowError(/Host value is not set/);
-    expect(() => address("¬˚˜∆˙¨√√†®ç†®∂œƒçƒ∂ß®´ß`®´£¢´®¨¥†øˆ¨ø(*&*ˆ%&ˆ%$#%@!")).toThrowError(/malformed/);
-    expect(() => address("123.45.43.56234", undefined)).toThrowError(/malformed/);
+    it("Ports below 1024 should be invalid", () => {
+      parsePort("0");
+      expect(mockLoggerError).toBeCalled();
+    });
+    it("Ports above 49151 should be invalid", () => {
+      parsePort("98765");
+      expect(mockLoggerError).toBeCalled();
+    });
+    it("Non-number ports should be invalid", () => {
+      parsePort("not a number");
+      expect(mockLoggerError).toBeCalled();
+    });
+    it("Ports between 1024 - 49151 should be valid", () => {
+      const port = parsePort("1984");
+      expect(port).toBe(1984);
+    });
   });
 
-  it("should handle valid URIs", () => {
-    expect(address("foo", undefined)).toBe("http://foo");
-    expect(address("foo.com", undefined)).toBe("http://foo.com");
-    expect(address("foo.com", 80)).toBe("http://foo.com");
-    expect(address("foo.com", "4200")).toBe("http://foo.com:4200");
-    expect(address("127.0.0.1", "4200")).toBe("http://127.0.0.1:4200");
-    expect(address("127.0.0.1", "4200")).toBe("http://127.0.0.1:4200");
-    expect(address("[::1]", "4200")).toBe("http://[::1]:4200");
+  describe("address()", () => {
+    it("should throw for malformed URI", () => {
+      expect(() => address("", undefined)).toThrowError(/Host value is not set/);
+      expect(() => address("", 80)).toThrowError(/Host value is not set/);
+      expect(() => address("¬˚˜∆˙¨√√†®ç†®∂œƒçƒ∂ß®´ß`®´£¢´®¨¥†øˆ¨ø(*&*ˆ%&ˆ%$#%@!")).toThrowError(/malformed/);
+      expect(() => address("123.45.43.56234", undefined)).toThrowError(/malformed/);
+    });
+
+    it("should handle valid URIs", () => {
+      expect(address("foo", undefined)).toBe("http://foo");
+      expect(address("foo.com", undefined)).toBe("http://foo.com");
+      expect(address("foo.com", 80)).toBe("http://foo.com");
+      expect(address("foo.com", "4200")).toBe("http://foo.com:4200");
+      expect(address("127.0.0.1", "4200")).toBe("http://127.0.0.1:4200");
+      expect(address("127.0.0.1", "4200")).toBe("http://127.0.0.1:4200");
+      expect(address("[::1]", "4200")).toBe("http://[::1]:4200");
+    });
+
+    it("should accept protocols: HTTP, HTTPS and WS protocols", () => {
+      expect(address("127.0.0.1", "4200", "http")).toBe("http://127.0.0.1:4200");
+      expect(address("127.0.0.1", "4200", "https")).toBe("https://127.0.0.1:4200");
+      expect(address("127.0.0.1", "4200", "ws")).toBe("ws://127.0.0.1:4200");
+    });
   });
 
-  it("should accept protocols: HTTP, HTTPS and WS protocols", () => {
-    expect(address("127.0.0.1", "4200", "http")).toBe("http://127.0.0.1:4200");
-    expect(address("127.0.0.1", "4200", "https")).toBe("https://127.0.0.1:4200");
-    expect(address("127.0.0.1", "4200", "ws")).toBe("ws://127.0.0.1:4200");
-  });
-});
+  describe("hostnameToIpAdress()", () => {
+    it("should recognize locahost", () => {
+      expect(hostnameToIpAdress("localhost")).toBe("127.0.0.1");
+    });
 
-describe("hostnameToIpAdress()", () => {
-  it("should recognize locahost", () => {
-    expect(hostnameToIpAdress("localhost")).toBe("127.0.0.1");
-  });
+    it("should recognize locahost", () => {
+      expect(hostnameToIpAdress("localhost")).toBe("127.0.0.1");
+    });
 
-  it("should recognize locahost", () => {
-    expect(hostnameToIpAdress("localhost")).toBe("127.0.0.1");
-  });
+    it("should ignore IP addresses", () => {
+      expect(hostnameToIpAdress("192.168.0.0")).toBe("192.168.0.0");
+    });
 
-  it("should ignore IP addresses", () => {
-    expect(hostnameToIpAdress("192.168.0.0")).toBe("192.168.0.0");
-  });
-
-  it("should ignore host names", () => {
-    expect(hostnameToIpAdress("foo.bar")).toBe("foo.bar");
+    it("should ignore host names", () => {
+      expect(hostnameToIpAdress("foo.bar")).toBe("foo.bar");
+    });
   });
 });
