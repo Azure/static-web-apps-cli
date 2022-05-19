7ed3344bb0ee083f28e17fc927717008a7f16358
sinedied <noda@free.fr>
2022-05-19 10:06:57 +0200

    refactor: fix typo

--- src/cli/commands/docs.ts
+++ src/cli/commands/docs.ts
@@ -4,7 +4,7 @@ import open from "open";
 export default function registerDocs(program: Command) {
   program
     .command("docs")
-    .description("Open Azure Static Web Apps CLI documentations")
+    .description("open Azure Static Web Apps CLI documentations")
     .action(async () => {
       await open("https://azure.github.io/static-web-apps-cli/");
     });
