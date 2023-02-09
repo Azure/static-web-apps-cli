b964f49ead4c751a4e4e5061cd8226dba47a49eb
sgollapudi77 <85578033+sgollapudi77@users.noreply.github.com>
2023-02-09 14:52:23 +0530

    fix: rename dab binary to new binary Name

--- src/core/dataApiBuilder/dab-main.ts
+++ src/core/dataApiBuilder/dab-main.ts
@@ -1,13 +1,14 @@
+import { DATA_API_BUILDER_BINARY_NAME } from "../constants";
 import { logger } from "../utils";
 import { installAndGetDataApiBuilder } from "./dab";
 
 /**
- * Ideally this function should get the dab binary path and run it
+ * Ideally this function should get the data Api Builder binary path and run it
  */
 
 export async function getDataApiBuilderBinaryPath(): Promise<string> {
   const binary = await installAndGetDataApiBuilder();
-  logger.silly(`DAB.exe found at ${binary.binaryPath}`);
+  logger.silly(`${DATA_API_BUILDER_BINARY_NAME} found at ${binary.binaryPath}`);
 
   return binary.binaryPath;
 }
